13ce0e3ef97624e106c71f5583207b83f19f92b3
Agustin Rodriguez <m.agustinrodriguez00@gmail.com>
2026-05-04 09:52:53 -0300

    cambios menores

--- 01-csLogica/tp1-avanced.cs/obj/Debug/net10.0/tp1-avanced.cs.AssemblyInfo.cs
+++ 01-csLogica/tp1-avanced.cs/obj/Debug/net10.0/tp1-avanced.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp1-avanced.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5a1afc36c7f4e56ed03b68491ac0657c667705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493e39a19623d5fc2ed49dc03b06055bdc9cfc3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp1-avanced.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp1-avanced.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tp-2/tp2.js
@@ -0,0 +1,21 @@
+//Serrano
+//Burgos
+//Rodriguez
+
+const readline = require('readline');
+
+const alumnos = []
+
+alumnos.push({apellido: "Albarracin", nota: 10, faltas: 10 })
+alumnos.push({apellido: "Amado", nota:1 ,faltas:9})
+alumnos.push({apellido: "Andrada", nota:8 ,faltas:8})
+alumnos.push({apellido: "Banegas", nota:2 ,faltas:7})
+alumnos.push({apellido: "Bascari", nota:9 ,faltas:6})
+alumnos.push({apellido: "Bravo", nota:3 ,faltas:5})
+alumnos.push({apellido: "Calderon", nota:7 ,faltas:4})
+alumnos.push({apellido: "Carrizo", nota:4 ,faltas:3})
+alumnos.push({apellido: "Chazarreta", nota:6, faltas:2})
+alumnos.push({apellido: "Diarte", nota:8 ,faltas:1})
+alumnos.push({apellido: "Diaz", nota:9 ,faltas:9})
+alumnos.push({apellido: "Diez", nota:10 ,faltas:10})
+console.log(alumnos)
